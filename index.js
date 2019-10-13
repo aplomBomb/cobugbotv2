20aab52630ee8ce60c2255afef64e1fb7c78aadc
Christian <seizethesteez@gmail.com>
2019-10-13 08:46:28 -0400

    randomized feedback upon request

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const insults = require("./insults.json");
 const redditEngine = require("./lib/redditEngine");
 const stats = require("./lib/stats");
 
-let randomNumber = Math.floor(Math.random() * Math.floor(insults.length));
+const searchFeedback = [
+  "🔎*looking around...*",
+  "🧲*attracting results...*",
+  "📡*contacting nasa...*",
+  "📻*analyzing airwaves...*",
+  "📞*making a call...*",
+  "🔮*looking into the future*",
+  "🔦*checking in all the scary spots...*",
+  "⏱*taking as long as I heckin' want...*",
+  "🔬*researching...*"
+];
+
+let randomInsult = Math.floor(Math.random() * Math.floor(insults.length));
 
 client.on("ready", () => {
   console.log(`${client.user.tag} is online and ready to hurt your feelings`);
@@ -23,66 +35,83 @@ client.on("ready", () => {
   console.log(`Bot is in ${serverCount.length} servers`);
 });
 
-client.on("message", async message => {
-  const prefix = "//";
-  // If the author's a bot, return
-  // If the message was not sent in a server, return
-  // If the message doesn't start with the prefix, return
-  if (message.author.bot) return;
-  if (!message.guild) return;
-  if (!message.content.startsWith(prefix)) return;
+client.on("message", message => {
+  (async () => {
+    const prefix = "//";
+    // If the author's a bot, return
+    // If the message was not sent in a server, return
+    // If the message doesn't start with the prefix, return
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
 
-  // Arguments and command variable
-  // cmd is the first word in the message, aka the command
-  // args is an array of words after the command
-  // !say hello I am a bot
-  // cmd == say (because the prefix is sliced off)
-  // args == ["hello", "I", "am", "a", "bot"]
-  const args = message.content
-    .slice(prefix.length)
-    .trim()
-    .split(/ +/g);
-  const cmd = args.shift().toLowerCase();
+    // Arguments and command variable
+    // cmd is the first word in the message, aka the command
+    // args is an array of words after the command
+    // !say hello I am a bot
+    // cmd == say (because the prefix is sliced off)
+    // args == ["hello", "I", "am", "a", "bot"]
+    const args = message.content
+      .slice(prefix.length)
+      .trim()
+      .split(/ +/g);
+    const cmd = args.shift().toLowerCase();
 
-  if (cmd === "servers") {
-    if (message.deletable) message.delete();
-    message.channel.send(`I am in ${serverCount.length} servers`);
-  }
-
-  if (cmd === "ping") {
-    // Send a message
-    if (message.deletable) message.delete();
-    const msg = await message.channel.send(`🏓 Pinging....`);
-
-    // Edit the message
-    msg.edit(`🏓 Pong!\nLatency is ${Math.round(client.ping)}ms`);
-  }
-
-  if (cmd === "insult") {
-    if (message.deletable) message.delete();
-
-    randomNumber === randomNumber + 1;
-
-    if (randomNumber > insults.length) {
-      randomNumber === 0;
+    if (cmd === "servers") {
+      if (message.deletable) message.delete();
+      message.channel.send(`I am in ${serverCount.length} servers`);
     }
 
-    if (message.content.includes("@")) {
-      const mention = message.mentions.users.find(user => user.username);
+    if (cmd === "ping") {
+      // Send a message
+      if (message.deletable) message.delete();
+      let msg = await message.channel.send(`🏓 Pinging....`);
 
-      message.channel.send(`${mention} ${insults[randomNumber].phrase}`);
-    } else {
-      message.reply(`${insults[randomNumber].phrase}`);
+      // Edit the message
+      msg.edit(`🏓 Pong!\nLatency is ${Math.round(client.ping)}ms`);
     }
-  }
 
-  //REDDIT LISTENERS////////////////////////////////////////////////////////////////////////
+    if (cmd === "insult") {
+      if (message.deletable) message.delete();
 
-  if (cmd === "dj" || "st" || "ihi" || "meme" || "gif" || "til") {
-    if (message.deletable) message.delete();
-    payload = await redditEngine.fetcher(cmd);
-    message.channel.send(payload);
-  }
+      randomInsult === randomInsult + 1;
+
+      if (randomInsult > insults.length) {
+        randomInsult === 0;
+      }
+
+      if (message.content.includes("@")) {
+        const mention = message.mentions.users.find(user => user.username);
+
+        message.channel.send(`${mention} ${insults[randomInsult].phrase}`);
+      } else {
+        message.reply(`${insults[randomInsult].phrase}`);
+      }
+    }
+
+    //REDDIT LISTENERS////////////////////////////////////////////////////////////////////////
+
+    if (
+      cmd === "dj" ||
+      cmd === "st" ||
+      cmd === "ihi" ||
+      cmd === "meme" ||
+      cmd === "gif" ||
+      cmd === "til"
+    ) {
+      let randomSearch = Math.floor(
+        Math.random() * Math.floor(searchFeedback.length)
+      );
+      try {
+        if (message.deletable) message.delete();
+        let msg = await message.channel.send(searchFeedback[randomSearch]);
+        payload = await redditEngine.fetcher(cmd);
+        msg.edit(payload);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  })().catch(console.log);
 });
 
 client.login(credentials.token);
