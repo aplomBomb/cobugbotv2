fa201a0bba39d58115d06afdf6ab848a853875d5
aplombomb <seizethesteez@gmail.com>
2019-10-13 12:22:10 -0400

    abstracted searchFiller array going to add way more

--- index.js
+++ index.js
@@ -1,55 +1,41 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({ disableEveryone: true });
-const credentials = require("./credentials");
-const insults = require("./insults.json");
-const redditEngine = require("./lib/redditEngine");
-const stats = require("./lib/stats");
+const Discord = require('discord.js')
+const client = new Discord.Client({ disableEveryone: true })
+const credentials = require('./credentials')
+const insults = require('./insults.json')
+const redditEngine = require('./lib/redditEngine')
+const stats = require('./lib/stats')
+const searchStatus = require('./lib/searchStatus')
 
-const searchFeedback = [
-  "🔎*looking around...*",
-  "🧲*attracting results...*",
-  "📡*contacting nasa...*",
-  "📻*analyzing airwaves...*",
-  "📞*making a call...*",
-  "🔮*looking into the future*",
-  "🔦*checking in all the scary spots...*",
-  "⏱*taking as long as I heckin' want...*",
-  "🔬*researching...*",
-  "📠*faxing your mom...*",
-  "🧭*getting bearings*",
-  "🔭*gazing into the vacuum of space...*",
-  "🛒*shopping for data...*",
-  "🔑*where did I put my keys...*",
-  "📫*snail-mail might be faster...*"
-];
+let randomInsult = Math.floor(Math.random() * Math.floor(insults.length))
 
-let randomInsult = Math.floor(Math.random() * Math.floor(insults.length));
-
-client.on("ready", () => {
-  console.log(`${client.user.tag} is online and ready to hurt your feelings`);
+client.on('ready', () => {
+  console.log(`${client.user.tag} is online and ready to hurt your feelings`)
 
   client.user.setPresence({
-    status: "Fapping",
+    status: 'Fapping',
     game: {
-      name: "getting developed",
-      type: "WATCHING"
-    }
-  });
+      name: 'getting developed',
+      type: 'WATCHING',
+    },
+  })
 
-  serverCount = client.guilds.map(name => name.name);
-  console.log(`Loaded ${insults.length} insults`);
-  console.log(`Bot is in ${serverCount.length} servers`);
-});
+  serverCount = client.guilds.map(name => name.name)
+  console.log(`Loaded ${insults.length} insults`)
+  console.log(`Bot is in ${serverCount.length} servers`)
+})
 
-client.on("message", message => {
-  (async () => {
-    const prefix = "//";
+client.on('message', message => {
+  ;(async () => {
+    const prefix = '//'
     // If the author's a bot, return
     // If the message was not sent in a server, return
     // If the message doesn't start with the prefix, return
-    if (message.author.bot) return;
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
+    if (message.author.bot) return
+    if (!message.guild) return
+    if (message.content.includes('525382819808280597')) {
+      message.react('🔪')
+    }
+    if (!message.content.startsWith(prefix)) return
 
     // Arguments and command variable
     // cmd is the first word in the message, aka the command
@@ -60,64 +46,81 @@ client.on("message", message => {
     const args = message.content
       .slice(prefix.length)
       .trim()
-      .split(/ +/g);
-    const cmd = args.shift().toLowerCase();
+      .split(/ +/g)
+    const cmd = args.shift().toLowerCase()
 
-    if (cmd === "servers") {
-      if (message.deletable) message.delete();
-      message.channel.send(`I am in ${serverCount.length} servers`);
+    if (cmd === 'servers') {
+      if (message.deletable) message.delete()
+      message.channel.send(`I am in ${serverCount.length} servers`)
     }
 
-    if (cmd === "ping") {
+    if (cmd === 'ping') {
       // Send a message
-      if (message.deletable) message.delete();
-      let msg = await message.channel.send(`🏓 Pinging....`);
+      try {
+        if (message.deletable) message.delete()
+        let msg = await message.channel.send(`🏓 Pinging....`)
 
-      // Edit the message
-      msg.edit(`🏓 Pong!\nLatency is ${Math.round(client.ping)}ms`);
+        // Edit the message
+        msg.edit(
+          `🏓 Pong!\n My latency with Discord API is ${Math.round(
+            client.ping,
+          )}ms`,
+        )
+      } catch (e) {
+        console.log(e)
+        message.author.send(
+          `**I need the proper permisssions to work, dumbass!**\n *...Read/Write/Manage messages*`,
+        )
+      }
     }
 
-    if (cmd === "insult") {
-      if (message.deletable) message.delete();
+    if (cmd === 'insult') {
+      if (message.deletable) message.delete()
 
-      randomInsult === randomInsult + 1;
+      randomInsult === randomInsult + 1
 
       if (randomInsult > insults.length) {
-        randomInsult === 0;
+        randomInsult === 0
       }
 
-      if (message.content.includes("@")) {
-        const mention = message.mentions.users.find(user => user.username);
+      if (message.content.includes('@')) {
+        const mention = message.mentions.users.find(user => user.username)
 
-        message.channel.send(`${mention} ${insults[randomInsult].phrase}`);
+        message.channel.send(`${mention} ${insults[randomInsult].phrase}`)
       } else {
-        message.reply(`${insults[randomInsult].phrase}`);
+        message.reply(`${insults[randomInsult].phrase}`)
       }
     }
 
     //REDDIT LISTENERS////////////////////////////////////////////////////////////////////////
 
     if (
-      cmd === "dj" ||
-      cmd === "st" ||
-      cmd === "ihi" ||
-      cmd === "meme" ||
-      cmd === "gif" ||
-      cmd === "til"
+      cmd === 'dj' ||
+      cmd === 'st' ||
+      cmd === 'ihi' ||
+      cmd === 'meme' ||
+      cmd === 'gif' ||
+      cmd === 'til'
     ) {
       let randomSearch = Math.floor(
-        Math.random() * Math.floor(searchFeedback.length)
-      );
+        Math.random() * Math.floor(searchStatus.collection.length),
+      )
+
       try {
-        if (message.deletable) message.delete();
-        let msg = await message.channel.send(searchFeedback[randomSearch]);
-        payload = await redditEngine.fetcher(cmd);
-        msg.edit(payload);
+        if (message.deletable) message.delete()
+        let msg = await message.channel.send(
+          searchStatus.collection[randomSearch],
+        )
+        payload = await redditEngine.fetcher(cmd)
+        msg.edit(payload)
       } catch (e) {
-        console.log(e);
+        console.log(e)
+        message.author.send(
+          `**I need the proper permisssions to work, dumbass!**\n *...Read/Write/Manage messages*`,
+        )
       }
     }
-  })().catch(console.log);
-});
+  })().catch(console.log)
+})
 
-client.login(credentials.token);
+client.login(credentials.token)
